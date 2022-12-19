17e81d03b494a612a381505384175a645d5cc12e
Maxim Alyoshin <mgreen097@gmail.com>
2022-12-19 19:04:01 +0300

    chore: add header

--- src/app/ui/header/header.tsx
+++ src/app/ui/header/header.tsx
@@ -1,5 +1,10 @@
+import { Container } from '../../../shared/ui/container';
 import styles from './header.module.css';
 
 export function Header() {
-  return <div className={styles.header}></div>;
+  return (
+    <div className={styles.header}>
+      <Container>header</Container>
+    </div>
+  );
 }
