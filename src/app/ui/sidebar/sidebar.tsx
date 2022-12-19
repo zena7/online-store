590c6d976ecab07da535cc0f81af37e8835f3a71
Maxim Alyoshin <mgreen097@gmail.com>
2022-12-19 19:05:23 +0300

    chore(ui): add sidebar component

--- src/app/ui/sidebar/sidebar.tsx
+++ src/app/ui/sidebar/sidebar.tsx
@@ -1,5 +1,5 @@
 import styles from './sidebar.module.css';
 
 export function Sidebar() {
-  return <aside className={styles.sidebar}></aside>;
+  return <aside data-layout="sidebar" className={styles.sidebar}></aside>;
 }
