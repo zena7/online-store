32540ef52049a1740220b1ea6bc0bc0661614a2d
Maxim Alyoshin <mgreen097@gmail.com>
2022-12-19 18:56:54 +0300

    chore: add react-router-dom

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,21 @@
-import { Provider } from 'react-redux';
-import { Routes } from './routes';
-import { store } from '../shared/store';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from '../pages/home';
+import BasketPage from '../pages/basket';
 
 import 'normalize.css';
+import './app.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/basket',
+    element: <BasketPage />,
+  },
+]);
 
 export function App() {
-  return (
-    <Provider store={store}>
-      <Routes />
-    </Provider>
-  );
+  return <RouterProvider router={router} />;
 }
