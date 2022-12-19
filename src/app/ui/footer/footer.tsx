99eab6af335c05f093c3d09c845548e4010a7432
Maxim Alyoshin <mgreen097@gmail.com>
2022-12-19 19:03:24 +0300

    chore: add footer component

--- src/app/ui/footer/footer.tsx
+++ src/app/ui/footer/footer.tsx
@@ -1,5 +1,10 @@
+import { Container } from '../../../shared/ui/container';
 import styles from './footer.module.css';
 
 export function Footer() {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <Container>footer</Container>
+    </footer>
+  );
 }
