38f73c155bd19c7b14b5de39c686b73aae1bae50
Maxim Alyoshin <mgreen097@gmail.com>
2022-12-19 19:05:05 +0300

    chore(ui): add main component

--- src/app/ui/main/main.tsx
+++ src/app/ui/main/main.tsx
@@ -1,5 +1,9 @@
 import styles from './main.module.css';
 
 export function Main() {
-  return <main className={styles.main}></main>;
+  return (
+    <main data-layout="main" className={styles.main}>
+      <h1>Hello</h1>
+    </main>
+  );
 }
