647edefc153c3f077847f36ec7b4855367ffdc9d
Nataly Zernova <nz_18@mail.ru>
2022-12-21 13:55:32 +0300

    refactor: add container component to error-page

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,14 +1,15 @@
 import { useRouteError } from 'react-router-dom';
+import { Container } from '../shared/ui/container';
 
 function ErrorPage() {
   const error = useRouteError() as any;
 
   return (
-    <div className="error-page">
+    <Container>
       <h1>404</h1>
       <p>That page was not found!</p>
       <p> {error.statusText || error.message}</p>
-    </div>
+    </Container>
   );
 }
 
