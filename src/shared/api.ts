f6d24230ffc563d66617b23ce18f0da74928dbde
Nataly Zernova <nz_18@mail.ru>
2022-12-21 17:54:42 +0300

    refactor: add optional parameter

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -1,25 +1,25 @@
 const API_URL = '';
 
 const api = {
-  get: <T = any>(url: string, options: RequestInit): Promise<T> =>
+  get: <T = any>(url: string, options?: RequestInit): Promise<T> =>
     fetch(`${API_URL}/${url}`, {
       ...options,
       method: 'GET',
     }).then((response) => response.json()),
 
-  post: <T = void>(url: string, options: RequestInit): Promise<T> =>
+  post: <T = void>(url: string, options?: RequestInit): Promise<T> =>
     fetch(`${API_URL}/${url}`, {
       ...options,
       method: 'POST',
     }).then((response) => response.json()),
 
-  delete: <T = void>(url: string, options: RequestInit): Promise<T> =>
+  delete: <T = void>(url: string, options?: RequestInit): Promise<T> =>
     fetch(`${API_URL}/${url}`, {
       ...options,
       method: 'DELETE',
     }).then((response) => response.json()),
 
-  put: <T = void>(url: string, options: RequestInit): Promise<T> =>
+  put: <T = void>(url: string, options?: RequestInit): Promise<T> =>
     fetch(`${API_URL}/${url}`, {
       ...options,
       method: 'PUT',
