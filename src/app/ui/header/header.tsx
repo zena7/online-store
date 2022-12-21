e41ec463d8011483ae58f8db1794be5373f9197c
Nataly Zernova <nz_18@mail.ru>
2022-12-21 14:23:20 +0300

    refactor: replace strings with constants in link component

--- src/app/ui/header/header.tsx
+++ src/app/ui/header/header.tsx
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom';
 import { Container } from '../../../shared/ui/container';
+import { CONFIG } from '../../../shared/libraries/router-config';
 import styles from './header.module.css';
 
 export function Header() {
   return (
     <div className={styles.header}>
       <Container>
-        <Link to="/">HOME</Link>
+        <Link to={CONFIG.HOME.LINK}>HOME</Link>
         <p>total</p>
-        <Link to="/basket">BASKET</Link>
+        <Link to={CONFIG.BASKET.LINK}>BASKET</Link>
       </Container>
     </div>
   );
