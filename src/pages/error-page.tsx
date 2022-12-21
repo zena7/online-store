ea61b42e48ede0ce30f2debd517c789ddb63cf36
Nataly Zernova <nz_18@mail.ru>
2022-12-21 13:54:01 +0300

    refactor: improve error-page component

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,14 +1,13 @@
 import { useRouteError } from 'react-router-dom';
 
 function ErrorPage() {
-  const error = useRouteError();
-  console.error('MY ERROR', error);
+  const error = useRouteError() as any;
 
   return (
     <div className="error-page">
       <h1>404</h1>
       <p>That page was not found!</p>
-      {/* <p> {error.statusText || error.message}</p> */}
+      <p> {error.statusText || error.message}</p>
     </div>
   );
 }
