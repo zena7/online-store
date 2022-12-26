e46e97f0ee75861203baf242af0d5024f45e59d6
Nataly Zernova <nz_18@mail.ru>
2022-12-26 16:46:56 +0300

    chore: create fetchProducts

--- src/entities/product/api.ts
+++ src/entities/product/api.ts
@@ -1,6 +1,15 @@
 import api from '../../shared/api';
 import { Product } from './types';
 
-export function getProducts() {
-  return api.get<Product[]>('/products');
+type FetchProductsResponse = {
+  limit: number;
+  products: Product[];
+  skip: number;
+  total: number;
+};
+
+export function fetchProducts() {
+  return api
+    .get<FetchProductsResponse>('/products/category/home-decoration')
+    .then((data) => data.products);
 }
