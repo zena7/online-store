40e3d9328b6063cf10e589d4473ab0e9e0ea039b
Nataly Zernova <nz_18@mail.ru>
2022-12-28 01:19:44 +0300

    refactor: remove ProductCard component to folder entities

--- src/entities/product/product-card.tsx
+++ src/entities/product/product-card.tsx
@@ -1,14 +1,18 @@
-import { Product } from '../entities/product/types';
+import { Product } from './types';
+import styles from './product.module.css';
 
 interface ProductProps {
   product: Product;
 }
 
 export function ProductCard({ product }: ProductProps) {
-  console.log('Props', product.brand);
   return (
     <>
-      <img src={product.images[0]} alt={product.title} />
+      <img
+        src={product.images[0]}
+        alt={product.title}
+        className={styles.productImg}
+      />
       <p>{product.title}</p>
       <p>{product.brand}</p>
       <p>{product.price} $</p>
