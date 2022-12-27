30caa0f6fb4ed9a6b41955b313b2f1f42eeff9b6
Nataly Zernova <nz_18@mail.ru>
2022-12-28 01:15:12 +0300

    refactor: add tag 'li' into ProductList component

--- src/features/product-list.tsx
+++ src/features/product-list.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { List } from '../shared/ui/list';
 import { fetchProducts } from '../entities/product';
 import { Product } from '../entities/product/types';
-import { ProductCard } from './product-card';
+import { ProductCard } from '../entities/product/product-card';
 
 export function ProductList() {
   const [products, setProducts] = useState<Product[]>([]);
