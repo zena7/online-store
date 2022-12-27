f9a1316232b7cd0fb9047a12b124db517be2d3aa
Nataly Zernova <nz_18@mail.ru>
2022-12-27 17:24:55 +0300

    chore: create list component

---
+++ src/shared/ui/list/list.tsx
@@ -0,0 +1,13 @@
+import styles from './list.module.css';
+
+interface ListProps {
+  children: React.ReactNode;
+}
+
+export function List({ children }: ListProps) {
+  return (
+    <ul className={styles.list}>
+      <li>{children}</li>
+    </ul>
+  );
+}
