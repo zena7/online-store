5c51407ad78c0cf46b27e90a4a4c296a84239c44
Nataly Zernova <nz_18@mail.ru>
2022-12-29 21:35:10 +0300

    chore: change List component

--- src/shared/ui/list/list.tsx
+++ src/shared/ui/list/list.tsx
@@ -1,9 +1,13 @@
+import clsx from 'clsx';
 import styles from './list.module.css';
 
 interface ListProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export function List({ children }: ListProps) {
-  return <ul className={styles.list}>{children}</ul>;
+export function List({ children, className }: ListProps) {
+  return (
+    <ul className={clsx(styles.list, className && className)}>{children}</ul>
+  );
 }
