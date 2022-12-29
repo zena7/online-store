a11cb5eb57207e3a498272248b16ab434c6be326
Nataly <nz_18@mail.ru>
2022-12-29 23:51:59 +0300

    Shared/UI list (#28)
    * chore: add clsx
    
    * chore: create List component
    
    * refactor: change List component

---
+++ src/shared/ui/list/list.tsx
@@ -0,0 +1,13 @@
+import clsx from 'clsx';
+import styles from './list.module.css';
+
+export interface ListProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function List({ children, className }: ListProps) {
+  return (
+    <ul className={clsx(styles.list, className && className)}>{children}</ul>
+  );
+}
