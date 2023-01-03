a37470f63ff17f59d62f1931681eb27fb52b9b49
MishaMgla <98478673+MishaMgla@users.noreply.github.com>
2023-01-03 12:51:27 +0300

    Revert "chore: create Button UI component"

--- src/shared/ui/button/button.tsx
+++
@@ -1,8 +0,0 @@
-interface ButtonProps {
-  onClick: () => void;
-  children?: React.ReactNode;
-}
-
-export const Button = ({ onClick, children }: ButtonProps) => {
-  return <button onClick={onClick}>{children}</button>;
-};
