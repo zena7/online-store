f11f1b2f402ec8aafd1d3fbd2cde29b2daeb1a93
Nataly <nz_18@mail.ru>
2023-01-07 17:10:36 +0300

    Refactoring product list on redux (#52)
    * feat: add Provider to connect redux
    
    * chore: add productListApi.reducer to store
    
    * refactor: delete api.ts and index.ts
    
    * chore: create productListApi
    
    * chore: use hook useGetProductsQuery in react component(productList)
    
    * refactor: change type any to Product
    
    * refactor(product-list): corrected comments by pr
    
    * chore: create config
    
    Co-authored-by: Maxim Alyoshin <mgreen097@gmail.com>

--- src/shared/store.ts
+++ src/shared/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { basketSlice } from '../features/basket/basket-slice';
+import { productListApi } from '../features/product-list/api';
 
 export const store = configureStore({
   reducer: {
     basket: basketSlice.reducer,
+    [productListApi.reducerPath]: productListApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productListApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
