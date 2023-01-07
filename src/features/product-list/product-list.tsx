b0ccb39e299c0f84a5c0e1a4ed2889c43f3b235d
Nataly Zernova <nz_18@mail.ru>
2023-01-07 16:40:02 +0300

    refactor: change type any to Product

--- src/features/product-list/product-list.tsx
+++ src/features/product-list/product-list.tsx
@@ -2,13 +2,14 @@ import { List } from '../../shared/ui/list';
 import { ProductCard } from '../../entities/product/ui/product-card/product-card';
 import { useGetProductsQuery } from './product-listApi';
 import styles from './product-list.module.css';
+import { Product } from '../../entities/product/types';
 
 export function ProductList() {
   const { data: { products } = {} } = useGetProductsQuery('/');
 
   return (
     <List className={styles.list}>
-      {products?.map((product: any) => (
+      {products?.map((product: Product) => (
         <li className={styles.item} key={product.id}>
           <ProductCard
             images={product.images}
