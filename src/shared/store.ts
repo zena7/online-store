c6849429ef9fbfb17bffd3fdccb727180c6501f3
Nataly Zernova <nz_18@mail.ru>
2023-01-07 15:54:12 +0300

    chore: add productListApi.reducer to store

--- src/shared/store.ts
+++ src/shared/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { basketSlice } from '../features/basket/basket-slice';
+import { productListApi } from '../features/product-list/product-listApi';
 
 export const store = configureStore({
   reducer: {
     basket: basketSlice.reducer,
+    [productListApi.reducerPath]: productListApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productListApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
