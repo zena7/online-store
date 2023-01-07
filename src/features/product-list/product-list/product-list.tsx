ee3048e8b6eb023d9965dffc2383aa17a43f28f5
Mikhail Zhuk <mikhail.avangarde@gmail.com>
2023-01-07 22:24:53 +0300

    refactor: resolve conflicts

--- src/features/product-list/product-list/product-list.tsx
+++ src/features/product-list/product-list/product-list.tsx
@@ -1,10 +1,22 @@
 import { List } from '@/shared/ui/list';
+import { Product } from '@/entities/product/types';
 import { ProductCard } from '@/entities/product/ui/product-card/product-card';
 import { useFetchProductsQuery } from '../service';
 import styles from './styles.module.css';
+import { useDispatch } from 'react-redux';
+import { basketSlice } from '@/features/basket/basket-slice';
 
 export function ProductList() {
   const { data: { products } = {} } = useFetchProductsQuery();
+
+  const dispatch = useDispatch();
+
+  const handleAddClick = (product: Product) => () => {
+    dispatch(basketSlice.actions.addProduct(product));
+  };
+  const handleDropClick = (id: number) => () => {
+    dispatch(basketSlice.actions.dropProduct(id));
+  };
 
   return (
     <List className={styles.list}>
@@ -15,6 +27,8 @@ export function ProductList() {
             title={product.title}
             brand={product.brand}
             price={product.price}
+            onAddClick={handleAddClick(product)}
+            onDropClick={handleDropClick(product.id)}
           />
         </li>
       ))}
