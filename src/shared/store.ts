71bf68b8dd10a28506d2795e387877481cace816
Mikhail Zhuk <mikhail.avangarde@gmail.com>
2023-01-08 18:35:03 +0300

    feat: add persist to basket state

--- src/shared/store.ts
+++ src/shared/store.ts
@@ -1,15 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { basketSlice } from '@/features/basket/basket-slice';
 import { productListApi } from '@/features/product-list/service';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const rootReducer = combineReducers({
+  basket: basketSlice.reducer,
+  [productListApi.reducerPath]: productListApi.reducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['basket'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    basket: basketSlice.reducer,
-    [productListApi.reducerPath]: productListApi.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productListApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(productListApi.middleware),
 });
+
+export let persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
