7db38f5619603bebb908a94db1e90e3c0f1085cd
Maxim Alyoshin <mgreen097@gmail.com>
2023-01-09 17:44:37 +0300

    refactor(product-list): rewrite product-list

--- src/features/product-list/ui/product-list/index.tsx
+++ src/features/product-list/ui/product-list/index.tsx
@@ -1,26 +1,26 @@
 import { List } from '@/shared/ui/list';
 import { Product } from '@/entities/product/types';
-import { ProductCard } from '@/entities/product/ui/product-card/product-card';
-import { useFetchProductsQuery } from '../service';
+import { ProductCard } from '@/features/product-list/ui/product-card';
+import { useFetchProductsQuery } from '../../api';
 import styles from './styles.module.css';
 import { useDispatch } from 'react-redux';
-import { basketSlice } from '@/features/basket/basket-slice';
+import { basketSlice } from '@/features/basket/store';
 
 export function ProductList() {
-  const { data: { products } = {} } = useFetchProductsQuery();
-
   const dispatch = useDispatch();
+  const { data: { products = [] } = {} } = useFetchProductsQuery();
 
   const handleAddClick = (product: Product) => () => {
     dispatch(basketSlice.actions.addProduct(product));
   };
+
   const handleDropClick = (id: number) => () => {
     dispatch(basketSlice.actions.dropProduct(id));
   };
 
   return (
     <List className={styles.list}>
-      {products?.map((product) => (
+      {products.map((product) => (
         <li className={styles.item} key={product.id}>
           <ProductCard
             images={product.images}
