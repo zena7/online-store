80c0ed0f240eae4920690de778aeeecdbd3cb8a9
Mikhail Zhuk <mikhail.avangarde@gmail.com>
2023-01-09 15:47:39 +0300

    refactor: change persistor to const, move persistReducer func call to configureStore property

--- src/shared/store.ts
+++ src/shared/store.ts
@@ -23,10 +23,10 @@ const persistConfig = {
   whitelist: ['basket'],
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: persistReducer(persistConfig, rootReducer),
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
@@ -35,7 +35,7 @@ export const store = configureStore({
     }).concat(productListApi.middleware),
 });
 
-export let persistor = persistStore(store);
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
