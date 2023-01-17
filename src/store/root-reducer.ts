60a99172116ba8ce4d0eeab19015c74954f94136
Nataly Zernova <nz_18@mail.ru>
2023-01-18 00:11:34 +0300

    chore: add sortingSlice to rootReducer

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { basketSlice } from '@/features/basket/store';
 import { productListService } from '@/features/product-list';
+import { sortingSlice } from '@/shared/ui/select-form/store';
 
 export const rootReducer = combineReducers({
   basket: basketSlice.reducer,
   [productListService.reducerPath]: productListService.reducer,
+  sorting: sortingSlice.reducer,
 });
