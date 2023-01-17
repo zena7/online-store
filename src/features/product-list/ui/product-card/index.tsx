a405a8a0b2872ad67b9de9d85a592dd13561fcf3
Nataly Zernova <nz_18@mail.ru>
2023-01-17 14:08:05 +0300

    chore: create buttons to add or remove products to basket

--- src/features/product-list/ui/product-card/index.tsx
+++ src/features/product-list/ui/product-card/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/shared/ui/button';
 import { Product } from '@/entities/product/types';
 import styles from './styles.module.css';
+import { useState } from 'react';
 
 type ProductCardProps = Pick<
   Product,
@@ -19,17 +20,41 @@ export function ProductCard({
   onDropClick,
 }: ProductCardProps) {
   const computedTitle = title.length > 20 ? title.slice(0, 20) : title;
+  const [added, setAdded] = useState(false);
 
   return (
-    <div className={styles.card}>
-      <img src={images[0]} alt={title} className={styles.img} />
-      <div className={styles.description}>
-        <p className={styles.title}>{computedTitle}</p>
-        <p>{brand}</p>
-        <p className={styles.price}>{price} $</p>
+    <div className={styles.productContainer}>
+      <div className={styles.card}>
+        <img src={images[0]} alt={title} className={styles.img} />
+        <div className={styles.description}>
+          <p className={styles.title}>{computedTitle}</p>
+          <p>{brand}</p>
+          <p className={styles.price}>{price} $</p>
+        </div>
       </div>
-      <Button onClick={onAddClick}>Add</Button>
-      <Button onClick={onDropClick}>Drop</Button>
+      <div className={styles.containerBtns}>
+        {!added ? (
+          <Button
+            onClick={() => {
+              onAddClick();
+              setAdded(true);
+            }}
+            className={styles.btn}
+          >
+            Add
+          </Button>
+        ) : (
+          <Button
+            onClick={() => {
+              onDropClick();
+              setAdded(false);
+            }}
+            className={styles.btn}
+          >
+            Drop
+          </Button>
+        )}
+      </div>
     </div>
   );
 }
