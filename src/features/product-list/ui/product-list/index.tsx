5de9515508a807ba81d8e89a7f8e1fb143b02a1d
Nataly Zernova <nz_18@mail.ru>
2023-01-18 20:03:52 +0300

    refactor: delete superfluous styles and inputs

--- src/features/product-list/ui/product-list/index.tsx
+++ src/features/product-list/ui/product-list/index.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react';
 import { List } from '@/shared/ui/list';
 import { Product } from '@/entities/product/types';
 import { ProductCard } from '@/features/product-list/ui/product-card';
 import { useFetchProductsQuery } from '@/features/product-list/service';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { basketSlice } from '@/features/basket/store';
-import styles from './styles.module.css';
-import { useSelector } from 'react-redux';
 import { RootState } from '@/store/configure-store';
+import styles from './styles.module.css';
 
 type ProductListProps = {
   page: number;
@@ -29,7 +27,7 @@ export function ProductList({ page, limit, skip }: ProductListProps) {
   };
 
   if (isLoading) {
-    return <div>Loading</div>;
+    return <div>Loading...</div>;
   }
 
   if (products?.length === 0) {
@@ -53,24 +51,6 @@ export function ProductList({ page, limit, skip }: ProductListProps) {
 
   return (
     <>
-      <div className={styles.containerSort}>
-        {/* <form name="showNumbersOfProducts">
-          <label>
-            <select
-              name="sortingProducts"
-              id="sort-select"
-              value={limit}
-              className={styles.selectForm}
-              onChange={handleSetLimitProducts}
-            >
-              <option value="48"> Show 48 </option>
-              <option value="64">Show 64</option>
-              <option value="80">Show 80</option>
-              <option value="100">Show 100</option>
-            </select>
-          </label>
-        </form> */}
-      </div>
       <List className={styles.list}>
         {(sort.property !== '' ? sortProductlist() : products)
           .slice(skip, skip + limit)
