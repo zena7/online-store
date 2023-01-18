62f391e962f4c33ea446c7a8d433c1258cf2d538
Nataly Zernova <nz_18@mail.ru>
2023-01-18 20:05:03 +0300

    chore: add limit products through useSelector in main component

--- src/pages/home/ui/main/index.tsx
+++ src/pages/home/ui/main/index.tsx
@@ -3,20 +3,25 @@ import { ProductList } from '@/features/product-list';
 import { Pagination } from '@/shared/ui/pagination';
 import { SortingForm } from '@/shared/ui/select-form';
 import { useState } from 'react';
+import { RootState } from '@/store/configure-store';
+import { useSelector } from 'react-redux';
+
 import styles from './styles.module.css';
 
 export function Main() {
   const [page, setPage] = useState(0);
-  const [limit, setLimit] = useState(8);
+  const limit: number = useSelector((state: RootState) => state.sorting.limit);
+
   const total = 100;
   const skip = page * limit;
 
   const handleClickPrev = () => {
     setPage((prevState) => prevState - 1);
+    window.scrollTo(0, 0);
   };
   const handleClickNext = () => {
     setPage((prevState) => prevState + 1);
-    console.log('page', page);
+    window.scroll(0, 0);
   };
 
   return (
