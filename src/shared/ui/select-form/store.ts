ebe1f568eafb89c44a9ba6c3303169365ebb0bc2
Nataly Zernova <nz_18@mail.ru>
2023-01-21 18:02:58 +0300

    refactor: take out LIMIT constant

--- src/shared/ui/select-form/store.ts
+++ src/shared/ui/select-form/store.ts
@@ -1,16 +1,19 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { SortItem } from './select-form';
+import { SortItem } from './ui/select-form';
 
 type InitialStateProps = {
   limit: number;
   sort: SortItem;
+  search: string;
 };
 
+const LIMIT = 8;
 const initialState: InitialStateProps = {
-  limit: 8,
+  limit: LIMIT,
   sort: {
     sortProperty: '',
   },
+  search: '',
 };
 
 export const sortingSlice = createSlice({
@@ -23,8 +26,11 @@ export const sortingSlice = createSlice({
     setSort: (state, action: PayloadAction<SortItem>) => {
       state.sort = action.payload;
     },
+    setSearch: (state, action: PayloadAction<string>) => {
+      state.search = action.payload;
+    },
   },
 });
 
-export const { setSort, setLimit } = sortingSlice.actions;
+export const { setSort, setLimit, setSearch } = sortingSlice.actions;
 export default sortingSlice.reducer;
