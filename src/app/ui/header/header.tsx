4c945dcf5877ea25678de4e02910df58e965a4ee
MishaMgla <98478673+MishaMgla@users.noreply.github.com>
2023-01-21 18:52:00 +0300

    feat: create total amount of products in basket component (#70)
    Co-authored-by: Nataly <nz_18@mail.ru>

--- src/app/ui/header/header.tsx
+++ src/app/ui/header/header.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { Container } from '@/shared/ui/container';
 import { CONFIG } from '@/shared/libraries/router-config';
 import styles from './header.module.css';
+import { TotalAmountCounter } from '@/features/total-amount-counter/total-amount-counter';
 import { TotalPriceCounter } from '@/features/total-price-counter/total-price-counter';
 
 export function Header() {
@@ -10,6 +11,8 @@ export function Header() {
       <Container>
         <Link to={CONFIG.HOME.LINK}>HOME</Link>
         <div>
+          <p>total amount:</p>
+          <TotalAmountCounter />
           <p>total price:</p>
           <TotalPriceCounter />
         </div>
