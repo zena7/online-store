125ab6f6d954de177207c7259ee0f87bc4acbbf3
Nataly Zernova <nz_18@mail.ru>
2023-01-23 23:22:36 +0300

    refactor: refactor error component (add some styles)

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -1,14 +1,17 @@
 import { useRouteError } from 'react-router-dom';
 import { Container } from '@/shared/ui/container';
+import styles from './styles.module.css';
 
 function ErrorPage() {
   const error = useRouteError() as any;
 
   return (
-    <Container>
+    <Container className={styles.error}>
       <h1>404</h1>
-      <p>That page was not found!</p>
-      <p> {error.statusText || error.message}</p>
+      <div className={styles.discription}>
+        <p>This page was not found!</p>
+        <p> {error.statusText || error.message}</p>
+      </div>
     </Container>
   );
 }
