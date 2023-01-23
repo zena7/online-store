22f1d49c6ebd8638a25c36cde461e1bced901f32
Nataly Zernova <nz_18@mail.ru>
2023-01-24 00:10:35 +0300

    chore: remove const total to context

--- src/shared/ui/pagination/pagination.tsx
+++ src/shared/ui/pagination/pagination.tsx
@@ -1,7 +1,8 @@
-import { Button } from '@/shared/ui/button';
 import clsx from 'clsx';
+import { useContext } from 'react';
+import { Button } from '@/shared/ui/button';
+import { MainContext } from '@/pages/home/ui/main';
 import styles from './styles.module.css';
-import { Product } from '@/entities/product/types';
 
 export interface PaginationProps {
   handleClickPrev?: () => void;
@@ -9,7 +10,6 @@ export interface PaginationProps {
   className: string;
   limit: number;
   skip: number;
-  total: number;
 }
 
 export const Pagination: React.FC<PaginationProps> = ({
@@ -18,8 +18,8 @@ export const Pagination: React.FC<PaginationProps> = ({
   className,
   limit,
   skip,
-  total,
 }) => {
+  const { total } = useContext(MainContext);
   const isFirstPage = skip === 0;
   const isLastPage = limit + skip >= total;
 
