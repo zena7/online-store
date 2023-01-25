f07ff528d00b9bc11b8b5605af1a4b5e9917e0cf
Nataly Zernova <nz_18@mail.ru>
2023-01-25 21:30:00 +0300

    refactor: add information about authors and links

--- src/app/ui/footer/footer.tsx
+++ src/app/ui/footer/footer.tsx
@@ -4,7 +4,13 @@ import styles from './footer.module.css';
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <Container>footer</Container>
+      <Container>
+        <p>
+          MARKETT ENGINEERED BY <a href="https://github.com/zena7">ZENA7</a> &{' '}
+          <a href="https://github.com/MishaMgla">MISHAMGLA</a>
+        </p>
+        <p>© 2023 MARKETT. ALL RIGHTS RESERVED</p>
+      </Container>
     </footer>
   );
 }
