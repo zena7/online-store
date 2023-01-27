3bd49f149aafb689fc3dc82397ecba1605aff553
Nataly Zernova <nz_18@mail.ru>
2023-01-27 18:44:40 +0300

    style: add some styles

--- src/app/ui/footer/footer.tsx
+++ src/app/ui/footer/footer.tsx
@@ -4,10 +4,20 @@ import styles from './footer.module.css';
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <Container>
+      <Container className={styles.info}>
         <p>
-          MARKETT ENGINEERED BY <a href="https://github.com/zena7">ZENA7</a> &{' '}
-          <a href="https://github.com/MishaMgla">MISHAMGLA</a>
+          MARKETT ENGINEERED BY{' '}
+          <a href="https://github.com/zena7" target="_blank" rel="noreferrer">
+            ZENA7
+          </a>{' '}
+          &{' '}
+          <a
+            href="https://github.com/MishaMgla"
+            target="_blank"
+            rel="noreferrer"
+          >
+            MISHAMGLA
+          </a>
         </p>
         <p>© 2023 MARKETT. ALL RIGHTS RESERVED</p>
       </Container>
