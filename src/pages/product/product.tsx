0f47a7a350279cb9c398a9194e255f413bade7f0
Nataly Zernova <nz_18@mail.ru>
2023-02-03 21:36:09 +0300

    feat: add function to convert title and brand, add state from getProductById

--- src/pages/product/product.tsx
+++ src/pages/product/product.tsx
@@ -1,21 +1,25 @@
 import { Container } from '@/shared/ui/container';
 import { useParams } from 'react-router-dom';
 import { Product } from '@/entities/product/types';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { RootState } from '@/store/configure-store';
 import { basketSlice } from '@/features/basket/store';
 import { Button } from '@/shared/ui/button';
 import { useFetchSingleProductQuery } from '@/features/product-list/service';
 import styles from './styles.module.css';
+import { getProductById } from '@/shared/selector';
 
 const ProductPage = () => {
-  // const a = useSelector((state: RootState) => state.basket);
-
   const { id } = useParams<{ id: any }>();
   const { data, isLoading } = useFetchSingleProductQuery(id);
   const dispatch = useDispatch();
-  const [added, setAdded] = useState<boolean>(false);
+  const [added, setAdded] = useState(
+    useSelector((state) => getProductById(state, id)),
+  );
+
+  function convertData(arg: string) {
+    return `${arg[0].toUpperCase()}${arg.slice(1).toLowerCase()}`;
+  }
 
   const handleAddClick = (product: Product | undefined) => {
     product && dispatch(basketSlice.actions.addProduct(product));
@@ -25,43 +29,65 @@ const ProductPage = () => {
     dispatch(basketSlice.actions.dropProduct(id));
   };
 
+  const limitNumberOfImages = (imgs: string[] | undefined) =>
+    imgs?.slice(0, 3) ?? [];
+
+  const [src, setSrc] = useState<string | undefined>('');
+  useEffect(() => {
+    setSrc(data?.images[0]);
+  }, [data]);
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
     <Container>
+      <ul className={styles.breadcrumb}>
+        <li>Store / </li>
+        <li> {data && convertData(data.category)} / </li>
+        <li> {data && convertData(data.brand)} /</li>
+        <li className={styles.currentItem}>
+          {data && convertData(data.category)}
+        </li>
+      </ul>
       <div className={styles.productSingle}>
         <div className={styles.imgs}>
-          {data?.images.slice(0, 4).map((item, id) =>
-            id === 0 ? (
-              <div className={styles.mainImg}>
-                <img src={item} alt={data.title} key={id} />
-              </div>
-            ) : (
-              <img
-                src={item}
-                alt={data.title}
-                key={id}
-                className={styles.img}
-              />
-            ),
-          )}
+          {limitNumberOfImages(data?.images).map((item, index) => (
+            <img
+              src={item}
+              alt={data?.title}
+              key={index}
+              className={styles.img}
+              onClick={() => setSrc(item)}
+            />
+          ))}
+          <div className={styles.mainImg} onClick={handleToggle}>
+            {data && <img src={src} alt={data?.title} key={id} />}
+          </div>
         </div>
         <div className={styles.info}>
-          <h1>{data?.title}</h1>
-          <p>{data?.brand}</p>
+          <h1 className={styles.header}>{data && convertData(data.title)}</h1>
+          <p>{data && convertData(data.brand)}</p>
           <p>{data?.description}</p>
-          <p>{data?.price} $</p>
-          <Button
-            className={styles.btn}
-            onClick={() => {
-              added ? handleDropClick(id) : handleAddClick(data);
-              setAdded(!added);
-            }}
-          >
-            {added ? 'Drop' : 'Add'}
-          </Button>
+          <p className={styles.price}>{data?.price} $</p>
+          <div className={styles.containerBtn}>
+            <Button
+              className={`${styles.btn} ${styles.add}`}
+              onClick={() => {
+                added ? handleDropClick(id) : handleAddClick(data);
+                setAdded(!added);
+              }}
+            >
+              {added ? 'Drop' : 'Add'}
+            </Button>
+            <Button
+              className={`${styles.btn} ${styles.checkout}`}
+              onClick={() => {}}
+            >
+              Order checkout
+            </Button>
+          </div>
         </div>
       </div>
     </Container>
